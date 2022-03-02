9a0d9ac3217377ede2f030a65be6303dd1688ce4
Nirmit Sawhney <nirmitsawhney01@gmail.com>
2022-03-02 14:16:10 +0530

    final-commit

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+if(process.env.NODE_ENV!=='production'){
+    require('dotenv').config();
+}
+
 const express=require("express");
 const app=express();
+const cors=require("cors");
 
-app.get("/bfhl",(req,res)=>{
-    res.send("hello nirmit");
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Hey there! I am Nirmit Sawhney");
+})
+app.post("/bfhl",(req,res)=>{
+    const data=[...req.body.data];
+    let numbers=data.filter((v)=>!isNaN(v));
+    let alpha=data.filter((v)=>(/[a-zA-Z]/).test(v));
+    res.status(200).send({
+        "is_success":"true",
+        "user_id":"Nirmit_Sawhney_16112001",
+        "email":"nirmit1491.cse19@chitkara.edu.in",
+        "roll_number":"1910991491",
+        "numbers":numbers,
+        "alphabets":alpha
+    })
 })
 
 const PORT=process.env.PORT || 3000;
